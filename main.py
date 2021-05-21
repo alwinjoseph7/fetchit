79cbff182c10682c3afadddcbee823f7f1526c6f
Adithya M <78593579+adithya-m08@users.noreply.github.com>
2021-05-21 20:01:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import cv2
-cap = cv2.VideoCapture(0)
+import qr
+import colour_detection
+import navigate
+
+video = cv2.VideoCapture(0)
+colour,start,stop='',False,False
+
 while True:
-    ret, frame = cap.read()
-    cv2.imshow("feed", frame)
-    key = cv2.waitKey(30)
-    if key == 32:
+    _, frame = video.read()
+    cv2.imshow("Video",frame)
+
+    if(colour==''):
+        colour=qr.read(frame)
+        start=True
+
+    elif(start==True and stop==False):
+        navigate.nav(frame)
+        stop=colour_detection.detect(frame,colour)
+
+    elif(stop==True):
+        print("STOP")
+        video.release()
+        cv2.destroyAllWindows()
         break
-cap.release()
-cv2.destroyAllWindows() 
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        video.release()
+        cv2.destroyAllWindows()
+        break
