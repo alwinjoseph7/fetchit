c553fe316f000161eee7a5e70bdb2c6fd7a2ef06
Adithya M <78593579+adithya-m08@users.noreply.github.com>
2021-05-22 12:17:28 +0530

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,14 +1,10 @@
 import cv2
 import numpy as np
 
-def read():
-    video = cv2.VideoCapture(0)
-    while True:
-        _, frame = video.read()
-        data,_,_=cv2.QRCodeDetector().detectAndDecode(frame)
-        if(data!=''):
-            colour=data.split(":")[1]
-            video.release()
-            break
-    return colour
-
+def read(frame):
+    data,_,_=cv2.QRCodeDetector().detectAndDecode(frame)
+    if(data!=''):
+        colour=data.split(":")[1]
+        return colour
+    else:   
+        return ''
