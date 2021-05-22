499b8a0e629ef454ae61eaa05f8866a6b88e69eb
Adithya M <78593579+adithya-m08@users.noreply.github.com>
2021-05-22 12:18:07 +0530

    Update colour_detection.py

--- colour_detection.py
+++ colour_detection.py
@@ -2,35 +2,32 @@ import cv2
 import numpy as np
 import time
 
-video = cv2.VideoCapture(0)
+def detect(frame,colour):
+    hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)   
 
-while True:
-    _, frame = video.read()
-    #frame=cv2.imread("C:\\Users\\adith\\Documents\\Fetch It\\images\\purple.png")
-    hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    if(colour=='Brown'):
+        brown_lower = np.array([11, 61, 115], np.uint8)
+        brown_upper = np.array([31, 81, 195], np.uint8)
+        brown_mask = cv2.inRange(hsvFrame, brown_lower, brown_upper)
+        if(cv2.countNonZero(brown_mask)>2000):
+            return True
+        else:
+            return False
 
-    brown_lower = np.array([3, 154,  83], np.uint8)
-    brown_upper = np.array([23, 174, 163], np.uint8)
-    brown_mask = cv2.inRange(hsvFrame, brown_lower, brown_upper)
+    elif(colour=='Purple'):
+        purple_lower = np.array([115, 127, 215], np.uint8)
+        purple_upper = np.array([135, 147, 255], np.uint8)
+        purple_mask = cv2.inRange(hsvFrame, purple_lower, purple_upper)
+        if(cv2.countNonZero(purple_mask)>2000):
+            return True
+        else:
+            return False
 
-    purple_lower = np.array([100,  70, 130], np.uint8)
-    purple_upper = np.array([140,  100, 222], np.uint8)
-    purple_mask = cv2.inRange(hsvFrame, purple_lower, purple_upper)
-
-    green_lower = np.array([30, 70, 130], np.uint8) 
-    green_upper = np.array([70, 100, 255], np.uint8)
-    green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
-
-    cv2.imshow("original",frame)
-    cv2.imshow("purple",purple_mask)
-    cv2.imshow("green",green_mask)
-    cv2.imshow("brown",brown_mask)
-
-    if(cv2.countNonZero(green_mask)>2000 or cv2.countNonZero(purple_mask)>2000 or cv2.countNonZero(purple_mask)>2000):
-        print("Stop")
-        break
-    
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        video.release()
-        cv2.destroyAllWindows()
-        break
+    elif(colour=='Green'):
+        green_lower = np.array([64, 146, 161], np.uint8) 
+        green_upper = np.array([84, 166, 241], np.uint8)
+        green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
+        if(cv2.countNonZero(green_mask)>2000):
+            return True
+        else:
+            return False
