7ee4070df50be8509cc26c82090159b35c4d6402
Adithya M <adithyam866.com>
2021-06-03 19:41:32 +0530

    edits

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import qr
 import colour_detection
-import navigate
+import pathfollow
 import time
 import serial
 
@@ -10,49 +10,19 @@ colour,stop='',False,
 Ser = serial.Serial("/dev/ttyACM0", baudrate=9600)
 Ser.flush()
 
-while True:
-    flag, frame = video.read()
-    if not flag:
-        _, frame = video.read()
-    cv2.imshow("QR",frame)   
-    
-    print("\n\n--------------Waiting for QR Code--------------\n\n")
-    if(colour==''):
-        colour=qr.read(frame) 
-    else:
-        print("\n\n--------------Start:",colour,"--------------\n\n")
-        print("--------------Please Remove the QR code--------------")
-        cv2.destroyWindow("QR")
-        break
-
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        video.release()
-        cv2.destroyAllWindows()
-        break
-
-time.sleep(1)
-
 while True:    
     flag, frame = video.read()
     if not flag:
         _, frame = video.read()
     
     try:
-        d=navigate.nav(frame)
+        d=pathfollow.nav(frame)
         Ser.write(bytes(d,'utf-8'))
     except:
         print("Track Not Visible")
         if Ser.inWaiting>0:
-            Ser.write(b"S")
-            Ser.write(b"X")
-            
-    if(colour_detection.detect(frame,colour)):
-        print("\n\n--------------Stop--------------\n\n")
-        if Ser.inWaiting>0:
-            Ser.write(b"S")
-        video.release()
-        cv2.destroyAllWindows()
-        break
+            Ser.write(b"s")
+            #Ser.write(b"x")
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         video.release()
