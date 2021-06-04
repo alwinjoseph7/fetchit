2d8a68a55d7feffba348b92383ba7afdd2195a99
Adithya M <78593579+adithya-m08@users.noreply.github.com>
2021-06-04 08:45:37 +0530

    Update main-navigate.py
    new code from github

--- main-navigate.py
+++ main-navigate.py
@@ -1,56 +1,34 @@
 import cv2
-import serial
 import numpy as np
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    low_b = np.uint8([8, 210, 152])
+    high_b = np.uint8([28, 230, 232])
+    mask = cv2.inRange(frame, high_b, low_b)
+    contours, hierarchy = cv2.findContours(mask, 1, cv2.CHAIN_APPROX_NONE)
+    if len(contours) > 0 :
+        c = max(contours, key=cv2.contourArea)
+        M = cv2.moments(c)
+        if M["m00"] !=0 :
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            print("CX : "+str(cx)+"  CY : "+str(cy))
+            if cx >= 120 :
+                print("Turn Left")
+            if cx < 120 and cx > 40 :
+                print("On Track!")
+            if cx <=40 :
+                print("Turn Right")
+            cv2.circle(frame, (cx,cy), 5, (255,255,255), -1)
+    else:
+        print("not")
+    #cv2.drawContours(frame, c, -1, (0,255,0), 1)
+    cv2.imshow("Mask",mask)
+    cv2.imshow("Frame",frame)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
 
-video = cv2.VideoCapture(0)
-Ser = serial.Serial("/dev/ttyACM0", baudrate=9600)
-Ser.flush()
 
-while True:    
-    flag, imageFrame = video.read()
-    if not flag:
-        _, imageFrame = video.read()
 
-    linecolor = (100, 215, 255)
-    
-    hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
-
-    lower = np.array([4, 106, 105], np.uint8)
-    upper = np.array([28, 255, 255], np.uint8)
-    mask = cv2.inRange(hsvFrame, lower, upper)
-
-    kernal = np.ones((5, 5), "uint8")
-    
-    mask = cv2.dilate(mask, kernal)
-    res1 = cv2.bitwise_and(imageFrame, imageFrame,mask = mask)
-    
-    res = cv2.GaussianBlur(res1,(5,5),0)
-
-    edged = cv2.Canny(res, 20, 50)
-    #cv2.imshow("edges", edged)
-    #cv2.imshow("mask", res)
-    cnts, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    
-    out = np.zeros_like (mask)
-    cv2.drawContours(imageFrame, [cnts], -1, color = 255, thickness = 2)
-
-    if len(cnts) > 0:
-
-        ((x, y), radius) = cv2.minEnclosingCircle(cnts[0])
-        M = cv2.moments(cnts[0])
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        if radius > 5:
-            cv2.circle(imageFrame, (int(x), int(y)), int(radius), (255, 255, 255), 2)
-            cv2.circle(imageFrame, center, 5, linecolor, -1)
-
-    cv2.imshow("video", imageFrame)
-
-    if x > 320:
-        print("\tRight")
-        Ser.write(b"R")
-    elif x < 280:
-        print("\tLeft")
-        Ser.write(b"L")
-    else: 
-        print("\tStraight")
-        Ser.write(b"F")
